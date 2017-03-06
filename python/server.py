d6b04a8c441af8610431d7db76d64cd6a4485fa4
Ryan M Bowen <rmb3518@gmail.com>
2017-03-06 13:22:53 -0500

    Fixed python server code to run with python3

--- python/server.py
+++ python/server.py
@@ -1,21 +1,20 @@
-import SocketServer
-import urlparse
+import socketserver
 import os
-import urllib
+from urllib.parse import urlparse
 from subprocess import Popen, PIPE
-from BaseHTTPServer import BaseHTTPRequestHandler
+from http.server import BaseHTTPRequestHandler
 import random
 import socket
 import sys
 
 class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        parsed = urlparse.urlparse(self.path)
+        parsed = urlparse(self.path)
         self.send_response(200)
         self.send_header("Content-Type", "text/html")
         self.end_headers()
-        self.wfile.write("done")
-        args = urlparse.parse_qs(parsed.query)
+        self.wfile.write(bytearray("done",'utf8'))
+        args = dict(query.split("=") for query in parsed.query.split("&"))
         print(self.path)
 
         handleCommand(args)
@@ -24,16 +23,17 @@ class MyHandler(BaseHTTPRequestHandler):
 def handleCommand(args):
     print(args)
     if "streamName" in args:
-	sock.sendall(args["streamName"][0])
+        sName = args["streamName"]
+        sock.send(sName.encode())
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Connect the socket to the port where the server is listening
 # Change server address and IP to be the correct versions
-server_address = ('192.168.1.1', 12345)
-print >>sys.stderr, 'connecting to %s port %s' % server_address
+server_address = ('10.42.42.114', 3181)
+print('connecting to %s port %s' % server_address,file=sys.stderr)
 sock.connect(server_address)
 
-port = 12345
-httpd = SocketServer.TCPServer(("", port), MyHandler)
+port = 1234
+httpd = socketserver.TCPServer(("", port), MyHandler)
 httpd.serve_forever()
